242b49db18d0abfb9b68e2c0adde5e6fe6100b07
Darya-Zhulitova <154079181+Darya-Zhulitova@users.noreply.github.com>
2024-06-13 01:40:46 +0300

    Add java code

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Operations.add(1, 2));
+        System.out.println(Operations.sub(2, 1));
+    }
+
+}
---
+++ src/main/java/Operations.java
@@ -0,0 +1,9 @@
+public class Operations {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
